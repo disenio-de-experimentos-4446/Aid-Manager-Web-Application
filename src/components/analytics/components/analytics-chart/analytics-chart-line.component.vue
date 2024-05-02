239991ba702bea9e9f9b1cf30d087293edfcc553
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-05-02 12:22:01 -0500

    feat(analytics-chart): added analytics-chart-line component

---
+++ src/components/analytics/components/analytics-chart/analytics-chart-line.component.vue
@@ -0,0 +1,87 @@
+<script>
+export default {
+  name: 'analytics-chart-line',
+  data() {
+    return {
+      chartData: null,
+      chartOptions: null
+    };
+  },
+  mounted() {
+    this.chartData = this.setChartData();
+    this.chartOptions = this.setChartOptions();
+  },
+  methods: {
+    setChartData() {
+      const documentStyle = getComputedStyle(document.documentElement);
+
+      return {
+        labels: ['lorem', 'lorem'],
+        datasets: [
+          {
+            label: 'Current',
+            data: [28, 48, 40, 19, 86, 27, 90],
+            fill: false,
+            backgroundColor: documentStyle.getPropertyValue('--green-300'),
+            borderColor: documentStyle.getPropertyValue('--green-300'),
+            tension: 0.4
+          },
+          {
+            label: 'Expected',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            backgroundColor: documentStyle.getPropertyValue('--green-500'),
+            borderColor: documentStyle.getPropertyValue('--green-500'),
+            tension: 0.4
+          }
+        ]
+      };
+    },
+    setChartOptions() {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      return {
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor
+            }
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary
+            },
+            grid: {
+              color: surfaceBorder
+            }
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary
+            },
+            grid: {
+              color: surfaceBorder
+            }
+          }
+        }
+      };
+    }
+  }
+};
+</script>
+<template>
+  <div class="card">
+    <pv-chart type="line" :data="chartData" :options="chartOptions" class="h-10rem" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
