d4001a18ea6e8a606cc9a57e5775dc37a145d041
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-05-02 12:22:13 -0500

    feat(analytics-chart): added analytics-chart-horizontal-bar component

---
+++ src/components/analytics/components/analytics-chart/analytics-chart-horizontal-bar.component.vue
@@ -0,0 +1,87 @@
+
+<template>
+  <div class="card">
+    <pv-chart type="bar" :data="chartData" :options="chartOptions" class="h-10rem"  />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'analytics-chart-horizontal-bar',
+  data() {
+    return {
+      chartData: null,
+      chartOptions: null
+    };
+  },
+  mounted() {
+    this.chartData = this.setChartData();
+    this.chartOptions = this.setChartOptions();
+  },
+  methods: {
+    setChartData() {
+      const documentStyle = getComputedStyle(document.documentElement);
+
+      return {
+        labels: ['Transportation', 'Food', 'Water'],
+        datasets: [
+          {
+            label: 'Current',
+            backgroundColor: documentStyle.getPropertyValue('--green-300'),
+            borderColor: documentStyle.getPropertyValue('--green-300'),
+            data: [28, 48, 40, 19, 86, 27, 90]
+          },
+          {
+            label: 'Expected',
+            backgroundColor: documentStyle.getPropertyValue('--green-500'),
+            borderColor: documentStyle.getPropertyValue('--green-500'),
+            data: [65, 59, 80, 81, 56, 55, 40]
+          }
+        ]
+      };
+    },
+    setChartOptions() {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      return {
+        indexAxis: 'y',
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor
+            }
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+              font: {
+                weight: 500
+              }
+            },
+            grid: {
+              display: false,
+              drawBorder: false
+            }
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary
+            },
+            grid: {
+              color: surfaceBorder,
+              drawBorder: false
+            }
+          }
+        }
+      };
+    }
+  }
+};
+</script>
