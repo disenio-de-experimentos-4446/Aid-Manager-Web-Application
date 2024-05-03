4f10ac6fa2ebbba1bf2cbcca38c6e16a0a9e518a
Arian Rodriguez <arianmrv12@gmail.com>
2024-05-02 21:54:02 -0500

    add: member entity

---
+++ src/models/team-member.entity.js
@@ -0,0 +1,9 @@
+class TeamMemberEntity {
+    constructor({ image, name, email }) {
+        this.image = image;
+        this.name = name;
+        this.email = email;
+    }
+}
+
+export default TeamMemberEntity;
