3bd5d8617153fb713a0b4794c80f69d0a205a9a5
JuanAlejandroCuadrosRodriguez <u20221a359@upc.edu.pe>
2025-06-11 22:20:22 -0500

    feat: github pages

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import svgLoader from 'vite-svg-loader'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    svgLoader()
-  ],
+  base: '/Aid-Manager-Web-Application/',
+  plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
