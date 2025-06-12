41bb3ac694f781a374b58d80e15c745a27be81b1
Alumnos <alumnos@A509-06.local>
2025-06-12 07:43:59 -0500

    added gtag in classroom

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 
 export default {
   name: 'App',
+  onMounted() {
+    const gtagId = process.env.VUE_APP_GTAG;
+    console.log('Google Analytics ID:', gtagId);
+    console.log('NODE_ENV:', process.env.NODE_ENV);
+  },
 }
 
 
